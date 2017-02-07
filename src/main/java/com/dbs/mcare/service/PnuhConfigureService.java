11c7450f58ef3a33cfab2d419730ca3ede91ce80
권혜은 <hyeeunkwon@idatabank.com>
2017-02-07 10:12:22 +0900

    temporary.password.length, 임시비밀번호 길이 삭제. temporary.password.level, 추가.

--- src/main/java/com/dbs/mcare/service/PnuhConfigureService.java
+++ src/main/java/com/dbs/mcare/service/PnuhConfigureService.java
@@ -1,17 +1,10 @@
 package com.dbs.mcare.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.dbs.mcare.framework.service.ConfigureService;
 
 @Component  
 public class PnuhConfigureService extends ConfigureService {
-	// 임시비밀번호 길이 
-	@Value("#{mcareConfig['temporary.password.length'] ?: '5'}")	
-	private int temporaryPasswordLength; 	
-	
-	public int getTemporaryPasswordLength() { 
-		return this.temporaryPasswordLength; 
-	}
+
 }
